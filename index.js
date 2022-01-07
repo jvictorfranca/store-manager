70d927fd8cdca372e885aff8a96199bbbfa1effc
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-01-07 16:27:45 -0300

    faz o requisito 1 aleluia senhor

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const { createProductController } = require('./controllers/products.controller');
+
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// app.get('/users', listUsers);
+app.post('/products', createProductController);
+
+app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
